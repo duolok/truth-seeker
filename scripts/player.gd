cf4648d352202a916b5fd68fd9434b843b38c3e5
OgnjenGligoric <87246330+OgnjenGligoric@users.noreply.github.com>
2025-02-08 19:12:43 +0100

    dialog

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@onready var actionable_finder: Area2D = $Direction/ActionableFinder
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,7 +14,15 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	if Input.is_action_just_pressed("ui_accept"):
+		var actionables = actionable_finder.get_overlapping_areas()
+		print(actionables)
+		if actionables.size() > 0:
+			print("overlapping")
+			actionables[0].action()
+			
+			
+			
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
