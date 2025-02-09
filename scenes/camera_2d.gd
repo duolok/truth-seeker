b3010a4c4f07bfd4d1226aa50703d337af652f1d
OgnjenGligoric <87246330+OgnjenGligoric@users.noreply.github.com>
2025-02-09 01:46:14 +0100

    camera smoothing

--- scenes/camera_2d.gd
+++ scenes/camera_2d.gd
@@ -7,12 +7,11 @@ func _ready() -> void:
 	var screen_size = get_viewport_rect().size
 	zoom = screen_size / Vector2(500, 200)
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var screen_size = get_viewport_rect().size
 	zoom = screen_size / Vector2(16 * 35, 9 * 35)
-
+	
 	if shake_amount > 0:
 		shake_amount = max(shake_amount - delta * 5, 0)  # Reduce shake over time
 		offset = Vector2(randf_range(-shake_amount, shake_amount), randf_range(-shake_amount, shake_amount))
