a2017975a2935675b9028409064cd1f9ee93639f
stefandjurica69 <stefandjurica69@gmail.com>
2025-02-09 14:17:03 +0100

    ker

--- scripts/teleport_area.gd
+++ scripts/teleport_area.gd
@@ -1,5 +1,9 @@
 extends Area2D
 
 func teleport(player: CharacterBody2D) -> void:
-	player.position.x = -122
-	player.position.y = -6
+	if player.position.x < 1900:
+		player.position.x = -716
+		player.position.y = -200
+	else:
+		player.position.x = -122
+		player.position.y = -6
