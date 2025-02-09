0c1e434f88915a1d48e15f916b0ff4c6426367ef
stefandjurica69 <stefandjurica69@gmail.com>
2025-02-09 07:15:37 +0100

    update: cica dijalog

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -1,10 +1,24 @@
 extends CharacterBody2D
 
+@onready var npc: CharacterBody2D = $"."
+@onready var animator: AnimatedSprite2D = $AnimatedSprite2D
 @export var dialogue_id: String
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-
+func _ready():
+	var hat_color = npc.get_meta("hat_color")
+	#default, red, gree, yellow : 0,1,2,3
+	match hat_color:
+		0:
+			animator.play("default")
+		1:
+			animator.play("npc_red")
+		2:
+			animator.play("npc_green")
+		3:
+			animator.play("npc_yellow")	
+		
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
