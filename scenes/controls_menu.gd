894929fa217780835dabe420e2c98b38d5cf1798
OgnjenGligoric <87246330+OgnjenGligoric@users.noreply.github.com>
2025-02-13 20:12:27 +0100

    feat: controls menu

--- scenes/controls_menu.gd
+++ scenes/controls_menu.gd
@@ -1,5 +1,6 @@
-extends CanvasLayer
+extends ScrollContainer
 
+@export var manu_to_back_to: Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,12 +10,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-func _on_quit_button_pressed() -> void:
-	get_tree().quit()
-	
-func _on_resume_button_pressed() -> void:
-	visible = false
-
-func _on_new_game_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/level0.tscn")
